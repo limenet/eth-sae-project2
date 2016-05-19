d0c4567e514845e95b889f061f3ca6fb54d8d6c5
Linus Metzler <linus.metzler@gmail.com>
2016-05-19 16:21:39 +0200

    new layout for outputs

--- src/Test1.java
+++ src/Test1.java
@@ -1,13 +1,11 @@
+// DIV_OUTPUT = NO_DIV_ZERO
+// BOUNDS_OUTPUT = NO_OUT_OF_BOUNDS
+
 /**
  * An example application you may want to analyze to test your analysis.
  *
  */
 public class Test1 {
-	
-	public static String DIV_OUTPUT = "NO_DIV_ZERO";
-	
-    public static String BOUNDS_OUTPUT = "NO_OUT_OF_BOUNDS";
-    
     public static void foo() {
         PrinterArray pa = new PrinterArray(5);
         pa.sendJob(2);
