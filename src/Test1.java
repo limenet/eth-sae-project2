7845435179ee0807f2033f488a16a66c3752edb9
Sandra Dünki <sandra@smx.ch>
2016-05-19 15:41:27 +0200

    code style

--- src/Test1.java
+++ src/Test1.java
@@ -3,10 +3,12 @@
  *
  */
 public class Test1 {
-    public static void foo() {        
-        public static String DIV_OUTPUT = "NO_DIV_ZERO";
-        public static String BOUNDS_OUTPUT = "NO_OUT_OF_BOUNDS";
-        
+	
+	public static String DIV_OUTPUT = "NO_DIV_ZERO";
+	
+    public static String BOUNDS_OUTPUT = "NO_OUT_OF_BOUNDS";
+    
+    public static void foo() {
         PrinterArray pa = new PrinterArray(5);
         pa.sendJob(2);
     }
