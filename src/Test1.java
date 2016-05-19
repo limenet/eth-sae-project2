a0a85cb21c3e2c9ca2f40ccbadd71dfd3583560f
Sandra Dünki <sandra@smx.ch>
2016-05-19 15:34:39 +0200

    added test output headers

--- src/Test1.java
+++ src/Test1.java
@@ -3,7 +3,10 @@
  *
  */
 public class Test1 {
-    public static void foo() {
+    public static void foo() {        
+        public static String DIV_OUTPUT = "NO_DIV_ZERO";
+        public static String BOUNDS_OUTPUT = "NO_OUT_OF_BOUNDS";
+        
         PrinterArray pa = new PrinterArray(5);
         pa.sendJob(2);
     }
