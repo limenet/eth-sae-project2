5e19d9e1547371a8373e65a5338056c56e5eecc4
Linus Metzler <linus.metzler@gmail.com>
2016-05-23 14:12:43 +0200

    renamed Test6, more tests

---
+++ src/TestMath.java
@@ -0,0 +1,23 @@
+// DIV_OUTPUT = MAY_DIV_ZERO
+// BOUNDS_OUTPUT = NO_OUT_OF_BOUNDS
+public class TestMath {
+	public static void foo() {
+		int i;
+
+		i = 1 / (2 + (-2));
+	}
+
+	public static void bar() {
+		int i = 0;
+
+		i = i / i;
+	}
+
+	public static void baz() {
+		int i = 1 + (2 + (3 + (4 + (5 / (-1 + 1)))));
+	}
+
+	public static void foobar(int n) {
+		int i = (1 + (2 + (3 + (4 + 5))) + n) / 0;
+	}
+}
