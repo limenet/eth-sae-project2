0a7f782d73f5abc2415532970b144551c6ba8652
Sandra Dünki <sandra@smx.ch>
2016-05-24 12:02:39 +0200

    Test9

---
+++ src/Test9.java
@@ -0,0 +1,14 @@
+// DIV_OUTPUT = NO_DIV_ZERO
+// BOUNDS_OUTPUT = NO_OUT_OF_BOUNDS
+
+// function call
+
+public class Test9 {
+	public static void foo() {
+		foo2();
+	}
+
+	public static void foo2() {
+		// I do nothing
+	}
+}
