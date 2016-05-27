c82007553409408e7cc6fb3954a36f39c6c4de3e
Andrina <adenzler@student.ethz.ch>
2016-05-27 14:16:21 +0200

    small changes in test case

--- src/TestApron.java
+++ src/TestApron.java
@@ -5,9 +5,12 @@
 
 public class TestApron {
 	public static void foo(int n) {
-		int a = 6;
+		int a = -6;
 		int b = a;
 		a = b * 5;
+		a = a * n;
+		b = 5 / n;
+		a = 2 - a;
 		a = b;
 		PrinterArray pa = new PrinterArray(10);
 		if ((a >= 0) && (a < 10)) {
