347aa8a34503ecb452e8508c23c6d2f543c0566c
Sandra Dünki <sandra@smx.ch>
2016-05-29 11:28:53 +0200

    Test 17 by timon

---
+++ src/Test17Timon.java
@@ -0,0 +1,13 @@
+// DIV_OUTPUT = NO_DIV_ZERO
+// BOUNDS_OUTPUT = NO_OUT_OF_BOUNDS
+
+public class Test17Timon {
+	public static void bar(int n) {
+		if (n == 0) {
+			int b = 0;
+		} else {
+			int b = 1 / n;
+		}
+
+	}
+}
