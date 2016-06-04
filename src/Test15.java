25bcb73514dd3be7417e578f635c4ccbe4a0fd63
Andrina <adenzler@student.ethz.ch>
2016-06-04 16:31:52 +0200

    fix expected test output

--- src/Test15.java
+++ src/Test15.java
@@ -1,9 +1,10 @@
 // DIV_OUTPUT = NO_DIV_ZERO
-// BOUNDS_OUTPUT = NO_OUT_OF_BOUNDS
+// BOUNDS_OUTPUT = MAY_OUT_OF_BOUNDS
 
 public class Test15 {
 	public static void foo(PrinterArray pa) {
-		pa.sendJob(0); // we can assume that pa is initialised with at least 1,
-						// therefore 0 is in the range
+		// we can assume that pa is initialised with at least 1, therefore 0 is
+		// in the range
+		pa.sendJob(0);
 	}
 }
