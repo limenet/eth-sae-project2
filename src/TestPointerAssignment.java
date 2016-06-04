7549d81b65c9a01101f8993f884a26bbdbf77a54
Andrina <adenzler@student.ethz.ch>
2016-06-04 22:57:54 +0200

    fix test case

--- src/TestPointerAssignment.java
+++ src/TestPointerAssignment.java
@@ -2,7 +2,6 @@
 // BOUNDS_OUTPUT = MAY_OUT_OF_BOUNDS
 
 class TestPointerAssignemnt {
-	/*
 	public static void foo() {
 		PrinterArray pa = new PrinterArray(10);
 		PrinterArray pb = new PrinterArray(20);
@@ -13,20 +12,5 @@ class TestPointerAssignemnt {
 			pc = pa;
 		}
 		pc.sendJob(25);
-	}
-	*/
-	public static void foo4() {
-		PrinterArray pa = new PrinterArray(20);
-		PrinterArray pb = new PrinterArray(21);
-		PrinterArray pc = new PrinterArray(22);
-		for (int i = 1; i <= 20000; i += 0.5) {
-			pa = pb; // pa cannot be referenced ever again; "lost"
-			pb = pc;
-			pc = pa; // pc point alternating to the original pb or pc; after an
-						// even number of iterations to original pc
-		}
-		// loop was executed an even number of times => pc should have intervall
-		// [0, 22-1]
-		pc.sendJob(21);
 	}
 }
